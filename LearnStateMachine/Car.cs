19befa22f45e80db25558634bcc1339d87e4cdee
Yash Shah <yshah@evolenthealth.com>
2022-08-16 07:18:43 -0400

    Add project files.

---
+++ LearnStateMachine/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace LearnStateMachine
+{
+    public class Car
+    {
+        public enum State 
+        {
+            Stopped,
+            Started,
+            Running
+        }
+        public enum Action 
+        {
+            Stop,
+            Start,
+            Accelerate
+        }
+    }
+}
---
+++ LearnStateMachine/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LearnStateMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var carStateMachine = new Stateless.StateMachine<Car.State, Car.Action>(Car.State.Stopped);
+
+            carStateMachine.Configure(Car.State.Stopped)
+                .Permit(Car.Action.Start, Car.State.Started);
+
+            var accelerateWithSpeed = carStateMachine.SetTriggerParameters<int>(Car.Action.Accelerate);
+
+            carStateMachine.Configure(Car.State.Started)
+                .Permit(Car.Action.Accelerate, Car.State.Running)
+                .Permit(Car.Action.Stop, Car.State.Stopped)
+                .OnEntry(s => Console.WriteLine($"On entry : source - {s.Source} & destination - {s.Destination}"))
+                .OnExit(s => Console.WriteLine($"On Exit : source - {s.Source} & destination - {s.Destination}"));
+
+            carStateMachine.Configure(Car.State.Running)
+                .SubstateOf(Car.State.Started)
+                .Permit(Car.Action.Stop, Car.State.Stopped)
+                .OnEntryFrom(accelerateWithSpeed, speed => Console.WriteLine($"speed : {speed}"))
+                .InternalTransition(Car.Action.Start, () => Console.WriteLine("Start called while running"));
+
+
+            Console.WriteLine($"Initial State: {carStateMachine.State}");
+
+            Console.WriteLine($"Invoke start action");
+            carStateMachine.Fire(Car.Action.Start);
+            Console.WriteLine($"New State After start action invoke: {carStateMachine.State}");
+
+            Console.WriteLine($"Invoke Accelerate action with 80 speed");
+            carStateMachine.Fire(accelerateWithSpeed,80);
+            Console.WriteLine($"New State After Accelerate action invoke: {carStateMachine.State}");
+
+            Console.WriteLine($"Invoke Start action");
+            carStateMachine.Fire(Car.Action.Start);
+            Console.WriteLine($"New State After Start action invoke: {carStateMachine.State}");
+
+            Console.WriteLine($"Invoke Stop action");
+            carStateMachine.Fire(Car.Action.Stop);
+            Console.WriteLine($"New State After Stop action invoke: {carStateMachine.State}");
+
+            Console.WriteLine(carStateMachine.IsInState(Car.State.Running));
+        }
+    }
+}
